bdd5a2ab234e09e2c03b70269be8f5ebbbef09b5
Matthew Behan-Fossey <matthewbf8@gmail.com>
2019-11-13 21:38:44 -0800

    Created weight fucntion

--- hammingDistance.js
+++ hammingDistance.js
@@ -5,9 +5,22 @@
                              [1,0,1,0,0,1,0,1], [0,1,0,1,1,1,1,1], [1,1,1,0,1,0,0,0], [0,1,1,1,0,0,0,1], [1,1,1,1,0,0,1,1],
                              [1,0,1,1,0,1,0,1], [1,0,0,1,1,1,1,1]
                             ]
-    hammingCodeBook.forEach(column => {
-        console.log("column is " + column.length + " long" + column)
-    })
+    //to find smallest Hamming weight, find the vector with the least # of 1s - could be multiple
+    var currentSmallestColumn = null
+    var smallestWeight = []
+    function countWeights(matrix){
+        var hash = []
+        for (var i = 0; i<matrix.length; i++){
+            var numOnes = 0
+            matrix[i].forEach(bit => {
+                if(bit == 1){numOnes ++}
+            })
+            hash.push({column: i, numOnes: numOnes})
+        }
+        return hash
+    }
 
-    console.log('Hello World!');
+
+
+    console.log(countWeights(hammingCodeBook));
 }());
