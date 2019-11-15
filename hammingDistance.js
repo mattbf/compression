69b2065d1de4f69e5a25bfe1d7b44cf5d235b94f
Matthew Behan-Fossey <matthewbf8@gmail.com>
2019-11-15 13:54:38 -0800

    Tested with (7,4) hamming code

--- hammingDistance.js
+++ hammingDistance.js
@@ -1,10 +1,14 @@
 (function main() {
     //matrix written as list of arrays where each array represents a column in the matrix
-    const hammingCodeBook = [[1,0,0,0,1,0,0,0], [0,1,0,0,0,0,0,1], [0,0,1,0,0,0,1,1],
+    const hammingCodeBook2 = [[1,0,0,0,1,0,0,0], [0,1,0,0,0,0,0,1], [0,0,1,0,0,0,1,1],
                              [0,0,0,1,0,1,0,1], [1,1,0,0,1,1,1,1], [0,1,1,0,1,0,0,0], [0,0,1,1,0,0,0,1], [1,1,0,1,0,0,1,1],
                              [1,0,1,0,0,1,0,1], [0,1,0,1,1,1,1,1], [1,1,1,0,1,0,0,0], [0,1,1,1,0,0,0,1], [1,1,1,1,0,0,1,1],
                              [1,0,1,1,0,1,0,1], [1,0,0,1,1,1,1,1]
                             ]
+
+    const hammingCodeBook = [
+[0,0,0,1,0,0,0], [0,1,0,0,1,0,1],[0,0,1,0,1,1,0], [0,0,0,1,0,1,1], [1,1,0,0,0,1,0], [1,0,1,0,0,0,1], [1,0,0,1,1,0,0], [0,1,1,0,0,1,1], [0,1,0,1,1,1,0], [0,0,1,1,1,0,1], [1,1,1,0,1,0,0], [1,1,0,1,0,0,1], [1,0,1,1,0,1,0], [0,1,1,1,0,0,0], [1,1,1,1,1,1,1],
+    ]
 
     var numOfColumns = 0
     hammingCodeBook.forEach(column => numOfColumns ++)
@@ -40,7 +44,7 @@
 
         function hammingDistance(a, b) {
           let distance = 0;
-            console.log("comparing " + a + " with " + b)
+            //console.log("comparing " + a + " with " + b)
           for (let i = 0; i < a.length; i += 1) {
             if (a[i] !== b[i]) {
               distance += 1;
@@ -58,7 +62,7 @@
                     let hamDist = hammingDistance(
                         hammingCodeBook[compColumn], hammingCodeBook[j]
                     )
-                    console.log("Comp Column " + compColumn + " has min ham dist " + hamDist)
+                    console.log("Comp Column " + compColumn + " has min ham dist " + hamDist + " with column " + j)
                     if(initDmin == null){
                         console.log("first dmin is " + hamDist)
                         initDmin = hamDist
