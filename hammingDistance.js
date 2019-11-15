c1d70fd2d489da45dec4ff9ac775dc951f14933c
Matthew Behan-Fossey <matthewbf8@gmail.com>
2019-11-14 16:51:56 -0800

    Added filter to weighting function so that it only returns the lowest wieghting vectors

--- hammingDistance.js
+++ hammingDistance.js
@@ -5,10 +5,38 @@
                              [1,0,1,0,0,1,0,1], [0,1,0,1,1,1,1,1], [1,1,1,0,1,0,0,0], [0,1,1,1,0,0,0,1], [1,1,1,1,0,0,1,1],
                              [1,0,1,1,0,1,0,1], [1,0,0,1,1,1,1,1]
                             ]
-    //to find smallest Hamming weight, find the vector with the least # of 1s - could be multiple
-    var currentSmallestColumn = null
-    var smallestWeight = []
+
+
     function countWeights(matrix){
+        var hash = []
+        for (var i = 0; i<matrix.length; i++){
+            var numOnes = 0
+            matrix[i].forEach(bit => {
+                if(bit == 1){numOnes ++}
+            })
+            //console.log("in column " + i + " there are " + numOnes)
+            //console.log(hash)
+            if(i == 0){
+                //first smallest weight
+                hash.push({column: i, numOnes: numOnes})
+            } else if(numOnes < hash[0].numOnes){
+                //clear and replace the hash - new smallest weight
+                hash = []
+                hash.push({column: i, numOnes: numOnes})
+            } else if (numOnes == hash[0].numOnes){
+                //add to hash
+                hash.push({column: i, numOnes: numOnes})
+            }
+        }
+
+        return hash
+    }
+
+
+    function findDmin(matrix){
+
+        //to find smallest Hamming weight, find the vector with the least # of 1s - could be multiple
+        function countWeights(){
         var hash = []
         for (var i = 0; i<matrix.length; i++){
             var numOnes = 0
@@ -18,6 +46,14 @@
             hash.push({column: i, numOnes: numOnes})
         }
         return hash
+    }
+
+        function dMin(weightHash){
+
+        }
+
+        return dMin(countWeights())
+
     }
 
 
